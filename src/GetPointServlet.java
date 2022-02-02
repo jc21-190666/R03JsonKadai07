fc8826cbbac3f459ab8043b21473eba4c8710bb2
柊斗 金賀 <190666@jc-21.jp>
2022-02-02 10:01:13 +0900

    ゲットポイント0202

--- src/GetPointServlet.java
+++ src/GetPointServlet.java
@@ -1,6 +1,11 @@
 
-
+import java.io.Console;
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,28 +13,134 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+/*
+import com.sun.jndi.ldap.Connection;
+import com.sun.net.httpserver.Authenticator.Result;
+import com.sun.tools.javac.util.List;
+*/
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Servlet implementation class GetPointServlet
  */
+
 @WebServlet("/getPoint")
 public class GetPointServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetPointServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
-		rd.forward(request, response);
+	public GetPointServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			response.setCharacterEncoding("utf-8");
+				String driverClassName = "com.mysql.jdbc.Driver";
+				String url ="jdbc:mysql://192.168.54.190:3306/jsonkadai07";
+				String user ="jsonkadai07";
+				String password ="JsonKadai07";
+				
+				
+				int point = 0;
+				
+				
+				try {
+					Class.forName(driverClassName);
+					Connection con = DriverManager.getConnection(url,user,password);
+					PreparedStatement st =
+							con.prepareStatement(
+									"select point from point_mst where  shop_id = ? and user_id = ?"
+								); 
+					PreparedStatement st2 =
+							con.prepareStatement(
+									"insert into point_mst values(?,?,500)"
+								); 
+					
+					String shop_id =request.getParameter("shop_id");
+					String user_id =request.getParameter("user_id");
+					
+										
+					st.setString(1, shop_id);
+					st.setString(2, user_id);			
+					ResultSet rs = st.executeQuery();
+					
+					if(rs.next() == true) {
+						 point = rs.getInt("point");
+					}else {
+						st2.setString(1,"shop_id");
+						st2.setString(2,"user_id");
+						
+						st2.executeUpdate();
+					}
+					/*
+					 * // SQL文の実行 PreparedStatement pstmt =
+					 * con.prepareStatement("select * from point_mst"); ResultSet rs =
+					 * pstmt.executeQuery();
+					 */
+					// 検索結果を表示
+					
+					
+					
+					// 後処理（リソースのクローズ）
+					rs.close();
+					st.close();
+					con.close();
+					
+				}catch (SQLException e ) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				
+				} catch (ClassNotFoundException e ) {
+					
+					e.printStackTrace();
+					
+				}
+
+				request.setAttribute("point", point);
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
+				rd.forward(request, response);
+	}
 }
+
+/*
+ * final String driverName = "TEAM07"; final String url =
+ * "jdbc:oracle:thin:@192.168.54.190:3306/pdborcl"; final String id =
+ * "jsonkadai07"; final String pass = "JsonKadai07";
+ * 
+ * try { Class.forName(driverName); java.sql.Connection
+ * connection=DriverManager.getConnection(url,id,pass); PreparedStatement st =
+ * connection.prepareStatement("select * from point_mst"); ResultSet result =
+ * st.executeQuery();
+ * 
+ * java.util.List<String[]> list = new ArrayList<>(); while(resulut.next()==
+ * true) { String[] s = new String[1]; s[0] = result.getString("kari");
+ * list.add(s); }
+ * 
+ * for(String[] s: list) { System.out.println(s[0]); }
+ * request.setAttribute("list", list); request.getRequestDispatcher("pass");
+ * 
+ * } catch (ClassNotFoundException e ) { // TODO 自動生成された catch ブロック
+ * e.printStackTrace(); } catch (SQLException e ) { // TODO 自動生成された catch ブロック
+ * e.printStackTrace(); }
+ * 
+ * String point ="POINT"; String num = "3580";
+ * 
+ * request.setAttribute("point", point); request.setAttribute("num", num);
+ * 
+ * RequestDispatcher rd =
+ * request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
+ * rd.forward(request, response);
+ */
